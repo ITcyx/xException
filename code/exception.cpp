7d1ba072400299c6eb8d288ba74af6fe9b177d4b
Andy Cai <1781953879@qq.com>
2021-02-10 22:59:07 +0800

    change encoding from gbk to utf-8 && modified exception_error&argument_error&operation_error&file_error && add file_open_error&file_read_error&file_write_error

--- code/exception.cpp
+++ code/exception.cpp
@@ -22,7 +22,10 @@ const std::string& x::exception_error::get_message() const noexcept
 
 x::exception_error& x::exception_error::operator=(const exception_error& other_exception_error) noexcept
 {
+	if (this == &other_exception_error)
+		return *this;
 	msg = other_exception_error.msg;
+	return *this;
 }
 
 
@@ -34,33 +37,37 @@ x::exception_error& x::exception_error::operator=(const exception_error& other_e
 
 
 
-x::argument_error::argument_error() noexcept :exception_error("Argument Error"), id(-1)
+x::argument_error::argument_error() noexcept :exception_error("Argument Error!"), id(-1)
+{}
+
+x::argument_error::argument_error(const int& argument_id) noexcept :exception_error("Argument Error!"), id(argument_id)
+{}
+
+x::argument_error::argument_error(const std::string& message, const int& argument_id) noexcept :exception_error(message), id(argument_id)
 {}
 
 x::argument_error::argument_error(const argument_error& other_argument_error) noexcept :exception_error(other_argument_error.msg), id(other_argument_error.id)
 {}
 
-explicit x::argument_error::argument_error(const int& argument_error_id) noexcept :exception_error("Argument Error"), id(argument_error_id)
-{}
-
-explicit x::argument_error::argument_error(const std::string& message, const int& argument_error_id) noexcept :exception_error(message), id(argument_error_id)
-{}
-
 x::argument_error::~argument_error() noexcept
 {
-	msg = "";
+	if (msg != "")
+		msg = "";
 	id = -1;
 }
 
-int x::argument_error::get_argument_error_id() noexcept
+int x::argument_error::get_argument_id() const noexcept
 {
 	return id;
 }
 
 x::argument_error& x::argument_error::operator=(const argument_error& other_argument_error) noexcept
 {
+	if (this == &other_argument_error)
+		return *this;
 	msg = other_argument_error.msg;
 	id = other_argument_error.id;
+	return *this;
 }
 
 
@@ -75,6 +82,8 @@ x::argument_error& x::argument_error::operator=(const argument_error& other_argu
 x::operation_error::operation_error() noexcept :exception_error("Operation Error!")
 {}
 
+x::operation_error::operation_error(const std::string& message) noexcept :exception_error(message)
+{}
 
 
 
@@ -84,5 +93,91 @@ x::operation_error::operation_error() noexcept :exception_error("Operation Error
 
 
 
-x::file_error::file_error() noexcept :exception_error("File Error!")
+
+x::file_error::file_error() noexcept :exception_error("File Error!"), fn("")
+{}
+
+x::file_error::file_error(int, const std::string& file_name) noexcept :exception_error("File Error!"), fn(file_name)
+{}
+
+x::file_error::file_error(const std::string& message, const std::string& file_name) noexcept :exception_error(message), fn(file_name)
+{}
+
+x::file_error::file_error(const file_error& other_file_error) noexcept :exception_error(other_file_error.msg), fn(other_file_error.fn)
+{}
+
+x::file_error::~file_error() noexcept
+{
+	if (fn != "")
+		fn = "";
+	if (msg != "")
+		msg = "";
+}
+
+const std::string& x::file_error::get_file_name() const noexcept
+{
+	return fn;
+}
+
+x::file_error& x::file_error::operator=(const file_error& other_file_error) noexcept
+{
+	if (this == &other_file_error)
+		return *this;
+	msg = other_file_error.msg;
+	fn = other_file_error.fn;
+	return *this;
+}
+
+
+
+
+
+
+
+
+
+
+x::file_open_error::file_open_error() noexcept :file_error("File Opern Error!")
+{}
+
+x::file_open_error::file_open_error(int, const std::string& file_name) noexcept :file_error("File Open Error!", file_name)
+{}
+
+x::file_open_error::file_open_error(const std::string& message, const std::string& file_name) noexcept :file_error(message, file_name)
+{}
+
+
+
+
+
+
+
+
+
+
+x::file_read_error::file_read_error() noexcept :file_error("File Read Error!")
+{}
+
+x::file_read_error::file_read_error(int, const std::string& file_name) noexcept :file_error("File Read Error!", file_name)
+{}
+
+x::file_read_error::file_read_error(const std::string& message, const std::string& file_name) noexcept :file_error(message, file_name)
+{}
+
+
+
+
+
+
+
+
+
+
+x::file_write_error::file_write_error() noexcept :file_error("File Write Error!")
+{}
+
+x::file_write_error::file_write_error(int, const std::string& file_name) noexcept :file_error("File Write Error!", file_name)
+{}
+
+x::file_write_error::file_write_error(const std::string& message, const std::string& file_name) noexcept :file_error(message, file_name)
 {}
--- code/exception.h
+++ code/exception.h
@@ -23,39 +23,138 @@ namespace x
 		exception_error& operator=(const exception_error& other_exception_error) noexcept;
 	};
 
+
+
+
+
+
+
+
+
+
+	// 参数异常错误类
 	class argument_error :public exception_error
 	{
-	// 参数错误
 	private:
 		int id;
 
 	public:
 		// 构造函数，拷贝构造函数，析构函数
 		argument_error() noexcept;
+		explicit argument_error(const int& argument_id) noexcept;
+		explicit argument_error(const std::string& message, const int& argument_id = -1) noexcept;
 		argument_error(const argument_error& other_argument_error) noexcept;
-		explicit argument_error(const int& argument_error_id) noexcept;
-		explicit argument_error(const std::string& message, const int& argument_error_id = -1) noexcept;
 		~argument_error() noexcept;
 
 		// 功能性函数
-		int get_argument_error_id() noexcept;  // 当没有指定哪个参数错误时，返回-1
+		int get_argument_id() const noexcept;  // 当没有指定哪个参数错误时，返回-1。返回0指的是第一个参数，以此类推
 
 		// 重载运算符
 		argument_error& operator=(const argument_error& other_argument_error) noexcept;
-
 	};
 
+
+
+
+
+
+
+
+
+
+	// 操作常错误类
 	class operation_error :public exception_error
 	{
 	public:
-		// 构造函数
+		// 构造函数、拷贝构造函数
 		operation_error() noexcept;
+		explicit operation_error(const std::string& message) noexcept;
 	};
 
+
+
+
+
+
+
+
+
+
+	// 文件异常错误类
 	class file_error :public exception_error
 	{
+	protected:
+		std::string fn;
+
 	public:
-		// 构造函数
+		// 构造函数、拷贝构造函数
 		file_error() noexcept;
+		file_error(int, const std::string& file_name) noexcept;
+		explicit file_error(const std::string& message, const std::string& file_name = "") noexcept;
+		file_error(const file_error& other_file_error) noexcept;
+		~file_error() noexcept;
+
+		// 功能性函数
+		const std::string& get_file_name() const noexcept;  // 当没有指定哪个文件错误时，返回空字符串""
+
+		// 重载运算符
+		file_error& operator=(const file_error& other_file_error) noexcept;
+	};
+
+
+
+
+
+
+
+
+
+
+	// 文件打开异常错误类
+	class file_open_error :public file_error
+	{
+	public:
+		// 构造函数、拷贝构造函数
+		file_open_error() noexcept;
+		file_open_error(int, const std::string& file_name) noexcept;
+		explicit file_open_error(const std::string& message, const std::string& file_name) noexcept;
+	};
+
+
+
+
+
+
+
+
+
+
+	// 文件读取异常错误类
+	class file_read_error :public file_error
+	{
+	public:
+		// 构造函数、拷贝构造函数
+		file_read_error() noexcept;
+		file_read_error(int, const std::string& file_name) noexcept;
+		explicit file_read_error(const std::string& message, const std::string& file_name) noexcept;
+	};
+
+
+
+
+
+
+
+
+
+
+	// 文件写入异常错误类
+	class file_write_error :public file_error
+	{
+	public:
+		// 构造函数、拷贝构造函数
+		file_write_error() noexcept;
+		file_write_error(int, const std::string& file_name) noexcept;
+		explicit file_write_error(const std::string& message, const std::string& file_name) noexcept;
 	};
 }
