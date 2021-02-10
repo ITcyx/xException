5623b2fcea2fe1a213c1966e9a48b1ae3c7efea6
Andy Cai <1781953879@qq.com>
2021-02-10 17:19:48 +0800

    modified exception_error

--- code/exception.h
+++ code/exception.h
@@ -1,23 +1,54 @@
 #pragma once
+#include <string>
 
 namespace x
 {
+	// 异常错误基类
 	class exception_error
 	{
-	// 异常错误基类
-	public:
-		exception_error() noexcept;
-		exception_error(const char* const message) noexcept;
-		exception_error(const exception_error& other_exception_error) noexcept;
-		exception_error& operator=(const exception_error& other_exception_error) noexcept;
-		virtual ~exception_error() noexcept;
-		virtual char const* MSG() noexcept;
 	private:
-		char const* msg;
+		std::string msg;
+
+	public:
+		// 构造函数，拷贝构造函数，析构函数
+		exception_error() noexcept;
+		explicit exception_error(const std::string& message) noexcept;
+		exception_error(const exception_error& other_exception_error) noexcept;
+		virtual ~exception_error() noexcept;
+
+		// 功能函数
+		virtual const std::string& get_message() const noexcept;
+
+		// 重载运算符
+		exception_error& operator=(const exception_error& other_exception_error) noexcept;
 	};
 
 	class argument_error :public exception_error
 	{
+	// 参数错误
+	private:
+		int id;
+	public:
+		// 构造函数，拷贝构造函数，析构函数
+		argument_error() noexcept;
+		argument_error(const argument_error& other_argument_error) noexcept;
+		explicit argument_error(const int& argument_error_id) noexcept;
+		explicit argument_error(const std::string& message, const int& argument_error_id = -1) noexcept;
 
+		~argument_error() noexcept;
+		int get_argument_error_id() noexcept;  // 当没有指定哪个参数错误时，返回-1
+
+
+	};
+
+	class operation_error :public exception_error
+	{
+	public:
+		const std::string& get_message() const noexcept;
+	};
+
+	class file_error :public exception_error
+	{
+		const std::string& get_message() const noexcept;
 	};
 }
