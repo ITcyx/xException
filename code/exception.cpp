c7585bd9abd2ffedd4e2c304f4e9cb0cf314e9ba
Andy Cai <1781953879@qq.com>
2021-02-12 12:31:29 +0800

    modified some details

--- code/exception.cpp
+++ code/exception.cpp
@@ -1,12 +1,16 @@
 # include "exception.h"
 
+
+// ********** exception_error 实现 **********
+
+// ---------- 构造、析构函数 ----------
 x::exception_error::exception_error() noexcept :msg("Unknown Error!")
 {}
 
-x::exception_error::exception_error(std::string const& message) noexcept :msg(message)
+x::exception_error::exception_error(exception_error const& other_exception_error) noexcept :msg(other_exception_error.msg)
 {}
 
-x::exception_error::exception_error(exception_error const& other_exception_error) noexcept :msg(other_exception_error.msg)
+x::exception_error::exception_error(std::string const& message) noexcept :msg(message)
 {}
 
 x::exception_error::~exception_error() noexcept
@@ -15,11 +19,13 @@ x::exception_error::~exception_error() noexcept
 		msg = "";
 }
 
+// ---------- 功能函数 ----------
 std::string const& x::exception_error::get_message() const noexcept
 {
 	return msg;
 }
 
+// ---------- 重载运算符 ----------
 x::exception_error& x::exception_error::operator=(exception_error const& other_exception_error) noexcept
 {
 	if (this == &other_exception_error)
@@ -37,16 +43,19 @@ x::exception_error& x::exception_error::operator=(exception_error const& other_e
 
 
 
+// ********** argument_error **********
+
+// ---------- 构造、析构函数 ----------
 x::argument_error::argument_error() noexcept :exception_error("Argument Error!"), id(-1)
+{}
+
+x::argument_error::argument_error(argument_error const& other_argument_error) noexcept :exception_error(other_argument_error.msg), id(other_argument_error.id)
 {}
 
 x::argument_error::argument_error(int const& argument_id) noexcept :exception_error("Argument Error!"), id(argument_id)
 {}
 
 x::argument_error::argument_error(std::string const& message, int const& argument_id) noexcept :exception_error(message), id(argument_id)
-{}
-
-x::argument_error::argument_error(argument_error const& other_argument_error) noexcept :exception_error(other_argument_error.msg), id(other_argument_error.id)
 {}
 
 x::argument_error::~argument_error() noexcept
@@ -56,11 +65,13 @@ x::argument_error::~argument_error() noexcept
 	id = -1;
 }
 
+// ---------- 功能性函数 ----------
 int x::argument_error::get_argument_id() const noexcept
 {
 	return id;
 }
 
+// ---------- 重载运算符 ----------
 x::argument_error& x::argument_error::operator=(argument_error const& other_argument_error) noexcept
 {
 	if (this == &other_argument_error)
@@ -79,6 +90,9 @@ x::argument_error& x::argument_error::operator=(argument_error const& other_argu
 
 
 
+// ********** operation_error **********
+
+// ---------- 构造函数 ----------
 x::operation_error::operation_error() noexcept :exception_error("Operation Error!")
 {}
 
@@ -94,6 +108,9 @@ x::operation_error::operation_error(std::string const& message) noexcept :except
 
 
 
+// ********** range_error **********
+
+// ---------- 构造函数 ----------
 x::range_error::range_error() noexcept :exception_error("Range Error!")
 {}
 
@@ -109,16 +126,19 @@ x::range_error::range_error(std::string const& message) noexcept :exception_erro
 
 
 
+// ********** file_error **********
+
+// ---------- 构造、析构函数 ----------
 x::file_error::file_error() noexcept :exception_error("File Error!"), fn("")
+{}
+
+x::file_error::file_error(file_error const& other_file_error) noexcept :exception_error(other_file_error.msg), fn(other_file_error.fn)
 {}
 
 x::file_error::file_error(int, std::string const& file_name) noexcept :exception_error("File Error!"), fn(file_name)
 {}
 
 x::file_error::file_error(std::string const& message, std::string const& file_name) noexcept :exception_error(message), fn(file_name)
-{}
-
-x::file_error::file_error(file_error const& other_file_error) noexcept :exception_error(other_file_error.msg), fn(other_file_error.fn)
 {}
 
 x::file_error::~file_error() noexcept
@@ -129,11 +149,13 @@ x::file_error::~file_error() noexcept
 		msg = "";
 }
 
+// ---------- 功能性函数 ----------
 std::string const& x::file_error::get_file_name() const noexcept
 {
 	return fn;
 }
 
+// ---------- 重载运算符 ----------
 x::file_error& x::file_error::operator=(file_error const& other_file_error) noexcept
 {
 	if (this == &other_file_error)
@@ -152,6 +174,9 @@ x::file_error& x::file_error::operator=(file_error const& other_file_error) noex
 
 
 
+// ********** file_open_error **********
+
+// ---------- 构造、析构函数 ----------
 x::file_open_error::file_open_error() noexcept :file_error("File Opern Error!")
 {}
 
@@ -169,7 +194,9 @@ x::file_open_error::file_open_error(std::string const& message, std::string cons
 
 
 
+// ********** file_read_error **********
 
+// ---------- 构造函数 ----------
 x::file_read_error::file_read_error() noexcept :file_error("File Read Error!")
 {}
 
@@ -188,6 +215,9 @@ x::file_read_error::file_read_error(std::string const& message, std::string cons
 
 
 
+// ********** file_write_error **********
+
+// ---------- 构造函数 ----------
 x::file_write_error::file_write_error() noexcept :file_error("File Write Error!")
 {}
 
--- code/exception.h
+++ code/exception.h
@@ -3,6 +3,7 @@
 
 namespace x
 {
+	// ********** exception_error **********
 	// 异常错误基类
 	class exception_error
 	{
@@ -10,16 +11,16 @@ namespace x
 		std::string msg;
 
 	public:
-		// 构造函数，拷贝构造函数，析构函数
+		// ---------- 构造、析构函数 ----------
 		exception_error() noexcept;
-		explicit exception_error(std::string const& message) noexcept;
 		exception_error(exception_error const& other_exception_error) noexcept;
+		explicit exception_error(std::string const& message) noexcept;
 		virtual ~exception_error() noexcept;
 
-		// 功能函数
+		// ---------- 功能函数 ----------
 		virtual std::string const& get_message() const noexcept;
 
-		// 重载运算符
+		// ---------- 重载运算符 ----------
 		exception_error& operator=(exception_error const& other_exception_error) noexcept;
 	};
 
@@ -32,6 +33,7 @@ namespace x
 
 
 
+	// ********** argument_error **********
 	// 参数异常错误类
 	class argument_error :public exception_error
 	{
@@ -39,17 +41,17 @@ namespace x
 		int id;
 
 	public:
-		// 构造函数，拷贝构造函数，析构函数
+		// ---------- 构造、析构函数 ----------
 		argument_error() noexcept;
+		argument_error(argument_error const& other_argument_error) noexcept;
 		explicit argument_error(int const& argument_id) noexcept;
 		explicit argument_error(std::string const& message, int const& argument_id = -1) noexcept;
-		argument_error(argument_error const& other_argument_error) noexcept;
 		~argument_error() noexcept;
 
-		// 功能性函数
+		// ---------- 功能性函数 ----------
 		int get_argument_id() const noexcept;  // 当没有指定哪个参数错误时，返回-1。返回0指的是第一个参数，以此类推
 
-		// 重载运算符
+		// ---------- 重载运算符 ----------
 		argument_error& operator=(argument_error const& other_argument_error) noexcept;
 	};
 
@@ -62,11 +64,12 @@ namespace x
 
 
 
+	// ********** operation_error **********
 	// 操作异常错误类
 	class operation_error :public exception_error
 	{
 	public:
-		// 构造函数、拷贝构造函数
+		// ---------- 构造函数 ----------
 		operation_error() noexcept;
 		explicit operation_error(std::string const& message) noexcept;
 	};
@@ -80,11 +83,12 @@ namespace x
 
 
 
+	// ********** range_error **********
 	// 越界异常错误类
 	class range_error :public exception_error
 	{
 	public:
-		// 构造函数、拷贝构造函数
+		// ---------- 构造函数 ----------
 		range_error() noexcept;
 		explicit range_error(std::string const& message) noexcept;
 	};
@@ -98,6 +102,7 @@ namespace x
 
 
 
+	// ********** file_error **********
 	// 文件异常错误类
 	class file_error :public exception_error
 	{
@@ -105,17 +110,17 @@ namespace x
 		std::string fn;
 
 	public:
-		// 构造函数、拷贝构造函数
+		// ---------- 构造、析构函数 ----------
 		file_error() noexcept;
+		file_error(file_error const& other_file_error) noexcept;
 		file_error(int, std::string const& file_name) noexcept;
 		explicit file_error(std::string const& message, std::string const& file_name = "") noexcept;
-		file_error(file_error const& other_file_error) noexcept;
 		~file_error() noexcept;
 
-		// 功能性函数
+		// ---------- 功能性函数 ----------
 		std::string const& get_file_name() const noexcept;  // 当没有指定哪个文件错误时，返回空字符串""
 
-		// 重载运算符
+		// ---------- 重载运算符 ----------
 		file_error& operator=(file_error const& other_file_error) noexcept;
 	};
 
@@ -128,11 +133,12 @@ namespace x
 
 
 
+	// ********** file_open_error **********
 	// 文件打开异常错误类
 	class file_open_error :public file_error
 	{
 	public:
-		// 构造函数、拷贝构造函数
+		// ---------- 构造、析构函数 ----------
 		file_open_error() noexcept;
 		file_open_error(int, std::string const& file_name) noexcept;
 		explicit file_open_error(std::string const& message, std::string const& file_name) noexcept;
@@ -147,11 +153,12 @@ namespace x
 
 
 
+	// ********** file_read_error **********
 	// 文件读取异常错误类
 	class file_read_error :public file_error
 	{
 	public:
-		// 构造函数、拷贝构造函数
+		// ---------- 构造函数 ----------
 		file_read_error() noexcept;
 		file_read_error(int, std::string const& file_name) noexcept;
 		explicit file_read_error(std::string const& message, std::string const& file_name) noexcept;
@@ -166,11 +173,12 @@ namespace x
 
 
 
+	// ********** file_write_error **********
 	// 文件写入异常错误类
 	class file_write_error :public file_error
 	{
 	public:
-		// 构造函数、拷贝构造函数
+		// ---------- 构造函数 ----------
 		file_write_error() noexcept;
 		file_write_error(int, std::string const& file_name) noexcept;
 		explicit file_write_error(std::string const& message, std::string const& file_name) noexcept;
